2708a90631c0ee2c0f72439a2fbcf516858a4f55
mirako77 <www@mickanel.net>
2020-02-20 16:13:17 +0100

    microComPOC project init
    init du projet microComPOC - un POC spring Boot

---
+++ SpringBoot/microComPOC/src/main/java/net/mickanel/microComPOC/MicroComPocApplication.java
@@ -0,0 +1,13 @@
+package net.mickanel.microComPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroComPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroComPocApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/microComPOC/src/test/java/net/mickanel/microComPOC/MicroComPocApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mickanel.microComPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroComPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
