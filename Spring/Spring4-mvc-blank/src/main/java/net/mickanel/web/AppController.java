01891e1ba6bb32047a00a3b647f3ec3bde2daf5d
mirako77 <spam@mickanel.net>
2021-02-28 09:03:59 +0100

    update to GetMapping
    utilisation de GetMapping introduit par Spring4.  RequestMapping n'est plus recommandé.
    
    Ajout page d'accueil

--- Spring/Spring4-mvc-blank/src/main/java/net/mickanel/web/AppController.java
+++ Spring/Spring4-mvc-blank/src/main/java/net/mickanel/web/AppController.java
@@ -5,9 +5,8 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import net.mickanel.service.InfoService;
@@ -25,7 +24,7 @@ public class AppController {
 //		this.helloWorldService = helloWorldService;
 //	}
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	@GetMapping(path = "/")
 	public String index(Map<String, Object> model) {
 
 		logger.debug("index() is executed!");
@@ -36,7 +35,7 @@ public class AppController {
 		return "index";
 	}
 	
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@GetMapping(value = "/hello")
 	public String hello(Map<String, Object> model) {
 
 		logger.debug("hello() is executed without name !");
@@ -44,7 +43,7 @@ public class AppController {
 		return "hello";
 	}
 
-	@RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+	@GetMapping(value = "/hello/{name:.+}")
 	public ModelAndView hello(@PathVariable("name") String name) {
 
 		logger.info("Entrer dans hello() is executed - $name {}", name);
